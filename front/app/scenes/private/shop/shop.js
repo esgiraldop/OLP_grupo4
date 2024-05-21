98f06521c085b1b9710ef40181d5a53b29c2cc33
Jeronimo <jerocruz76@gmail.com>
2024-05-21 01:10:17 -0500

    add html content to shop scene

--- front/app/scenes/private/shop/shop.js
+++ front/app/scenes/private/shop/shop.js
@@ -5,10 +5,24 @@ export function shopScene(){
     const pageContent = `
     <div class=${styles["generalContainer"]}>
         <div class=${styles["header"]}>
-                hola
+            <div class=${styles["title"]}>
+                <h2>
+                    X-Coins shop
+                </h2>
+            </div>
+            <button class${styles["moreInfo"]}>
+                More info.
+            </button>
         </div>
         <div class=${styles["shopContent"]}>
-
+            <div class=${styles["buyContent"]}>
+                <div class=${styles["shopNav"]}></div>
+                <div class=${styles["buyAvatar"]}></div>
+            </div>
+            <div class=${styles["availableContent"]}>
+                <div class=${styles["userAvatar"]}></div>
+                <div class=${styles["availableSide"]}></div>
+            </div>       
         </div>
     </div>
     `
